0438a949cb57aa362419e6192dadc2d1877bb82f
devozankara <dev.ozankara@gmail.com>
2024-12-07 22:00:01 +0300

    arithmeticoperators

---
+++ arithmeticoperators.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// (+ - 0 /) Basic matematik işlemleri gibi düşünülebilir!
+
+int main() {
+
+    int students = 20;
+
+    // students = students + 1; // bir yolu bu!
+
+    // students+=5; // bir başka yolu bu!
+
+    students++; // bir başka yolu bu <- bu sadece +1 ekler!
+
+    // Aynı durumlar - operatörleri içinde geçerlidir!
+
+    std::cout << students;
+
+    return 0;
+}
