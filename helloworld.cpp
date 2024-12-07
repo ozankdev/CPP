9ab9789f02941f993f6722fdea2ecf3f83baad61
devozankara <dev.ozankara@gmail.com>
2024-12-07 20:56:21 +0300

    initial-commit

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+    // commandLine
+    /*
+    Multiple
+    Command
+    Line
+    */
+
+    std::cout << "I Like Pizza!" << '\n';  // std (standard) -> cout (character out) << main operator!
+    std::cout << "It's really good!";
+
+    return 0;
+
+}
---
+++ variables.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+
+    int x = 5;
+    int y = 6;
+
+    int sum = x + y;
+
+
+    // std::cout << sum;
+
+    int age = 29;
+    int year = 2024;
+    int days = 7.5;
+    double days2 = 7.5;
+
+    // std::cout << days << std::endl;
+    // std::cout << days2;
+
+    char grade = '<';
+    char initial = 'B';
+
+    // std::cout << grade << std::endl;
+    // std::cout << initial;
+
+    bool student = false;
+    bool money = true;
+    bool car = false;
+
+    std::cout << student;
+
+    return 0;
+}
