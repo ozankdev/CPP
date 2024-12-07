8a3d7a0f22b483d2678e792e0788e6648ccedffa
devozankara <dev.ozankara@gmail.com>
2024-12-07 21:20:34 +0300

    Constants

---
+++ const.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    const double pi = 3.14159;
+    double radius = 10;
+    double circumference = 2 * pi * radius;
+
+    // std::cout << circumference << " cm";
+
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+
+    return 0;
+}
