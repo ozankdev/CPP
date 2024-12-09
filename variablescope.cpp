ab2af585cc34f82ea5ea1be9796817e29f0b0acd
devozankara <dev.ozankara@gmail.com>
2024-12-09 20:58:47 +0300

    improvements

---
+++ variablescope.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+void printNum();
+
+int main() {
+    printNum();
+    return 0;
+}
+
+void printNum() {
+    int myNum = 1;
+    std::cout << myNum;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Yerel değişkenler (local variables) = bir fonksiyonun veya kod bloğunun içinde tanımlanan değişkenlerdir.
+// Global değişkenler (global variables) = tüm fonksiyonların veya kod bloklarının dışında tanımlanmış değişkenlerdir.
